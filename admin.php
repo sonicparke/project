96e53aa42fa54483d84acc13abd370015f61eb80
Brad McAlister <github@sonicparke.com>
2012-05-18 23:35:44 -0500

    Made js file changes. Added admin.php

---
+++ admin.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>XPO Everything Finder Admin</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+  </head>
+  <body>
+    <div class="header">
+        <img src="images/logomergeb.gif" alt="XPO Press" />
+        <h1 class="title">XPO Everything Finder Admin</h1>
+    </div>
+
+    <div class="wrapperContent">
+      <div class="ui-widget">
+        <label for="vendors">Vendors: </label><input id="vendors">
+      </div>
+      <div class="ui-widget">
+        <label for="products">Products: </label><input id="products">
+      </div>
+      <div class="ui-widget">
+        <label for="shows">Shows: </label><input id="shows">
+      </div>
+    </div>
+
+
+
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="js/handlebars-1.0.0.beta.6.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
+    <script>
+        $(document).ready(function(e) {
+            var params = {
+                test: 'Hi'
+            }
+             
+        });
+    </script>
+  </body>
+</html>
