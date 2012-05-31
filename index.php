9ddfe6262806232c24765501cd59c6d82d51ccb5
Brad McAlister <github@sonicparke.com>
2012-05-31 00:23:56 -0500

    started IE CSS stuff

--- index.php
+++ index.php
@@ -6,14 +6,16 @@
     <link rel="stylesheet" type="text/css" href="css/jQuery-UI/jquery-ui-1.8.20.custom.css">
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <link href='http://fonts.googleapis.com/css?family=Nunito?family=Cabin:400,700' rel='stylesheet' type='text/css'>
+    <!--[if IE]>
+        <link rel="stylesheet" type="text/css" href="css/ie.css" />
+    <![endif]-->
   </head>
   <body>
     <div class="">
-        <div class="header">
-            <img src="images/logomergeb.gif" alt="XPO Press" />
-            <h1 class="title">XPO Everything Finder</h1>
-        </div>
-
+            <div class="header">
+                <img src="images/logo.png" alt="XPO Press" />
+                <h1 class="title">XPO Everything Finder</h1>
+            </div>
         <div class="wrapperContent">
             <div class="ui-widget searchForm container row clearfix">
                 <form>
